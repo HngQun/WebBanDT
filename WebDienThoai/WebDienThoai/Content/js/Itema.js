431ce3abdfffd9f3c7776ee90488929931efea59
KaMiz <quandang2812@gmal.com>
2022-12-23 15:59:21 +0700

    HngQun

---
+++ WebDienThoai/WebDienThoai/Content/js/Itema.js
@@ -0,0 +1,17 @@
+﻿
+
+function mOut(obj) {
+    obj.innerHTML = "22.000$";
+}
+
+function mOver(obj) {
+    obj.innerHTML = "Mua";
+}
+
+function checkphone(obj) {
+    var phone = document.getElementById('phoneNumber');
+    var filter = /^[0-9-+]+$/;
+    if (filter.test(phone) == false) {
+        alert('Nhập sai số điện thoại');
+    }
+}
---
+++ WebDienThoai/WebDienThoai/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDienThoai.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        public ActionResult PhuKien()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
